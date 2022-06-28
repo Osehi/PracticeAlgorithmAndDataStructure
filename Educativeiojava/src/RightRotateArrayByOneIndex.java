5b2a6152a3cfdb0610fd8683f46e23d2d9301058
Osehi <osehiaseehilen@gmail.com>
2022-06-28 11:27:55 +0100

    rotate-array-algorithm

---
+++ Educativeiojava/src/RightRotateArrayByOneIndex.java
@@ -0,0 +1,16 @@
+
+
+public class RightRotateArrayByOneIndex {
+    public static void main(String[] args) {
+        int[] scores = {1,2,3,4,5,6};
+        int startRotate = 0;
+        int numberOfRotate = 1;
+        while (startRotate < numberOfRotate){
+            for (int i = 0; i < scores.length; i++){
+                if (i < (scores.length - 1)){
+                    
+                }
+            }
+        }
+    }
+}
---
+++ educativeio/src/arrays/RightRotateArrayByOneIndex.kt
@@ -0,0 +1,41 @@
+package arrays
+
+fun main(){
+    var lines = arrayListOf<Int>(1,2,3,4,5,6)
+    lines[0] = lines[5]
+    println(lines)
+    println("here is an output: ${lines.indexOf(1)}")
+
+//    check()
+}
+
+fun check(){
+    var scores = arrayListOf<Int>(1, 2, 3, 4, 5)
+    val rotateByIndexOfOne = 1
+    var startPointForIndex = 0
+    var scoreStartPoint = 0
+    val scoresLength = scores.size
+    var oldElement = 0
+
+    // use two while loops
+    while (startPointForIndex < rotateByIndexOfOne){
+        // inner loop
+        for (i in 0 until scores.size){
+            if (i != (scoresLength - 1)){
+                println("this is the scoreIndex content: ${i}")
+                var indicator = i
+                println("here is the indicator: ${indicator}")
+                oldElement  = scores[indicator]
+                println("oldElement content: ${oldElement}")
+                scores[i + 1] = oldElement
+            } else {
+                scores[i - (scoresLength - 1)] = scores[scoresLength - 1]
+            }
+        }
+
+        startPointForIndex++
+    }
+
+    println(scores)
+}
+
