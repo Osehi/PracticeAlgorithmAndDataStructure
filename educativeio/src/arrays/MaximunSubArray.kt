8833113c163a359c861cd7ae0eb7d38eb8681e72
Osehi <osehiaseehilen@gmail.com>
2022-07-07 16:44:55 +0100

    maxsubarray-dynamic-programming-added

--- educativeio/src/arrays/MaximunSubArray.kt
+++ educativeio/src/arrays/MaximunSubArray.kt
@@ -1,7 +1,7 @@
 package arrays
 
 fun main(){
-    val scores = arrayListOf<Int>(1, 7, -2, -5, 10, -1)
+    val scores = arrayListOf<Int>(-4, 2, -5, 1, 2, 3, 6, -5, 1)
     var maxSum = Integer.MIN_VALUE
     var sum = 0
     for (i in scores.indices){
@@ -11,4 +11,25 @@ fun main(){
         }
     }
     println("The maximum sum is :${maxSum}")
+
+    println("This is the dynamic programming result : ${maxSubArray(scores)}")
+}
+
+/**
+ * This is the correct one that involves dynamic programming
+ */
+fun maxSubArray(list:ArrayList<Int>): Int{
+    var currMax = 0
+    var globalMax = 0
+    for (i in list.indices){
+        if (currMax < 0){
+            currMax = list[i]
+        } else {
+            currMax += list[i]
+        }
+        if (globalMax < currMax){
+            globalMax = currMax
+        }
+    }
+    return globalMax
 }
